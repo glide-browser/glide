a6d9767626c57982aaca7a27d22e7da330736074
Robert Craigie <robert@craigie.dev>
2025-05-23 19:19:32 +0100

    fix(internal): avoid macos specific sed usage

--- scripts/generate-types.sh
+++ scripts/generate-types.sh
@@ -20,16 +20,20 @@ pnpm tsc \
   ./engine/testing/modules/TestUtils.sys.mjs \
   ./engine/browser/base/content/browser-commands.js
 
+TEMP_FILE=$(mktemp)
+
 # our `.d.ts` files are intended to define global types so we can't use `export`
-sed -i '' 's/export namespace TestUtils {/declare namespace TestUtils {/' src/glide/generated/testing/modules/TestUtils.sys.d.mts
+sed 's/export namespace TestUtils {/declare namespace TestUtils {/' src/glide/generated/testing/modules/TestUtils.sys.d.mts > "$TEMP_FILE" && mv "$TEMP_FILE" src/glide/generated/testing/modules/TestUtils.sys.d.mts
 
 # remove existing `declare `s as they're not valid after we wrap everything in a `declare namespace`
-sed -i '' 's/^declare //g' src/glide/generated/testing/mochitest/tests/SimpleTest/EventUtils.d.ts
+sed 's/^declare //g' src/glide/generated/testing/mochitest/tests/SimpleTest/EventUtils.d.ts > "$TEMP_FILE" && mv "$TEMP_FILE" src/glide/generated/testing/mochitest/tests/SimpleTest/EventUtils.d.ts
+
 # add a namespace to properly emulate runtime behaviour
-sed -i '' '1i\
-declare namespace EventUtils {
-' src/glide/generated/testing/mochitest/tests/SimpleTest/EventUtils.d.ts
-echo -e "}" >> src/glide/generated/testing/mochitest/tests/SimpleTest/EventUtils.d.ts
+echo "declare namespace EventUtils {" > "$TEMP_FILE"
+cat src/glide/generated/testing/mochitest/tests/SimpleTest/EventUtils.d.ts >> "$TEMP_FILE"
+echo "}" >> "$TEMP_FILE"
+
+mv "$TEMP_FILE" src/glide/generated/testing/mochitest/tests/SimpleTest/EventUtils.d.ts
 
 mkdir -p src/glide/generated/@types
 cp engine/tools/@types/*.ts src/glide/generated/@types/
