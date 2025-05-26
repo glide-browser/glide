4a7e6e800d4b95c04a72cea0216ea72d75e2d416
Robert Craigie <robert@craigie.dev>
2025-05-26 10:58:26 +0100

    chore(preferences): point Glide support button to discussions

---
+++ src/glide/browser/components/preferences/glide.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const help = document.getElementById("helpButton");
+  help.setAttribute(
+    "href",
+    "https://github.com/glide-browser/glide/discussions"
+  );
+});
