41e53e43f84c93c117c28410d9b6fbab6d7a7106
Robert Craigie <robert@craigie.dev>
2025-06-18 08:13:48 -0400

    fix: avoid duplicate config loads on macos

--- src/glide/browser/base/content/glide-scripts.ts
+++ src/glide/browser/base/content/glide-scripts.ts
@@ -1,21 +1,35 @@
 // @ts-nocheck
 
-import("chrome://glide/content/browser.mjs").then(({ GlideBrowser }) => {
-  window.GlideBrowser = GlideBrowser;
-});
+// On MacOS, Firefox loads *two* top-level windows, one with the standard `browser.xhtml` frame
+// that is loaded on every platform and a separate hidden window:
+//
+// https://github.com/mozilla-firefox/firefox/blob/main/browser/base/content/hiddenWindowMac.xhtml
+//
+// I'm not quite sure *why* this happens, but disabling loading of our JS code in the hidden frame
+// doesn't seem to have any negative consequences yet.
+//
+// N.B. We disable it to avoid confusing duplicative logs / behaviour.
 
-import("chrome://glide/content/browser-commands.mjs").then(
-  ({ GlideCommands }) => {
-    window.GlideCommands = GlideCommands;
-  }
-);
+if (!window.location.toString().endsWith("hiddenWindowMac.xhtml")) {
+  import("chrome://glide/content/browser.mjs").then(({ GlideBrowser }) => {
+    window.GlideBrowser = GlideBrowser;
+  });
 
-import("chrome://glide/content/browser-excmds.mjs").then(({ GlideExcmds }) => {
-  window.GlideExcmds = GlideExcmds;
-});
+  import("chrome://glide/content/browser-commands.mjs").then(
+    ({ GlideCommands }) => {
+      window.GlideCommands = GlideCommands;
+    }
+  );
 
-import("chrome://glide/content/browser-excmds-registry.mjs").then(
-  ({ GLIDE_EXCOMMANDS }) => {
-    window.GLIDE_EXCOMMANDS = GLIDE_EXCOMMANDS;
-  }
-);
+  import("chrome://glide/content/browser-excmds.mjs").then(
+    ({ GlideExcmds }) => {
+      window.GlideExcmds = GlideExcmds;
+    }
+  );
+
+  import("chrome://glide/content/browser-excmds-registry.mjs").then(
+    ({ GLIDE_EXCOMMANDS }) => {
+      window.GLIDE_EXCOMMANDS = GLIDE_EXCOMMANDS;
+    }
+  );
+}
