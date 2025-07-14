61af3bafa5ca9cd1a7aad8b87900a31b98cd6a67
Robert Craigie <robert@craigie.dev>
2025-07-14 15:37:42 +0100

    chore(internal/lint): remove redundant config types check
    I realise now that my previous commit, fa4a51954ed979cfa5c0a12b76eff1f1ee183269,
    was incorrect in the sense that we were checking the config types I was
    just running the wrong command, but I think this new setup makes more
    sense anyway

--- scripts/check-types.sh
+++ scripts/check-types.sh
@@ -6,6 +6,8 @@ cd "$(dirname "$0")/.."
 
 pnpm tsc:browser
 
+echo "============ checking bundled config types ============"
 pnpm tsc:config
 
+echo "============ checking node.js script types ============"
 pnpm tsc:scripts
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,11 +3,5 @@
 set -e
 
 cd "$(dirname "$0")/.."
-tsc_path=$(realpath node_modules/.bin/tsc)
 
 pnpm tsc
-
-echo "============ checking bundled config types ============"
-
-./scripts/bundle-types.sh
-cd src/glide/browser/base/content/test/config/types && exec "$tsc_path"
