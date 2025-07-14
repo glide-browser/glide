7f5043af219c994b4a8e937aab6baac5fdd3fa36
Robert Craigie <robert@craigie.dev>
2025-07-14 15:39:09 +0100

    fix(internal/lint): bundle config types before checking them

--- scripts/check-types.sh
+++ scripts/check-types.sh
@@ -6,8 +6,12 @@ cd "$(dirname "$0")/.."
 
 pnpm tsc:browser
 
+echo "============ checking node.js script types ============"
+pnpm tsc:scripts
+
+echo "============ bundling config types         ============"
+./scripts/bundle-types.sh
+
 echo "============ checking bundled config types ============"
 pnpm tsc:config
 
-echo "============ checking node.js script types ============"
-pnpm tsc:scripts
