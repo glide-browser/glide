d73b9d6cfd1419e908676d04373a543a62cda5d5
Robert Craigie <robert@craigie.dev>
2025-07-15 20:44:59 +0100

    CI: build & sign macos x86

--- .github/scripts/macos-sign.sh
+++ .github/scripts/macos-sign.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 required_vars=(
+  "ARCH"
   "APPLE_DEVELOPER_ID"
   "APPLE_ACCOUNT_ID"
   "APPLE_TEAM_ID"
@@ -8,7 +9,7 @@ required_vars=(
   "P12_PASSWORD"
   "P12_BASE64"
   "PROVISIONING_PROFILE_BASE64"
-  "AARCH64_DMG_PATH"
+  "DMG_PATH"
 )
 
 missing_vars=()
@@ -26,7 +27,7 @@ if [[ ${#missing_vars[@]} -gt 0 ]]; then
   exit 1
 fi
 
-AARCH64_DMG_PATH=$(realpath $AARCH64_DMG_PATH)
+DMG_PATH=$(realpath $DMG_PATH)
 
 set -eux
 
@@ -42,14 +43,14 @@ cd engine
 
 step "Cleaning up"
 
-rm -rf "./obj-aarch64-apple-darwin/" || true
+rm -rf "./obj-$ARCH-apple-darwin/" || true
 
 step "Extracting .app"
-mkdir -p ./obj-aarch64-apple-darwin/dist
+mkdir -p ./obj-$ARCH-apple-darwin/dist
 
 ./mach python -m mozbuild.action.unpack_dmg \
-  $AARCH64_DMG_PATH \
-  ./obj-aarch64-apple-darwin/dist
+  $DMG_PATH \
+  ./obj-$ARCH-apple-darwin/dist
 
 step "Setting up credentials"
 echo "$P12_BASE64" > cert.txt
@@ -65,10 +66,10 @@ ls -la
 cp ./Glide_Browser.provisionprofile "./embedded.provisionprofile"
 
 step "Cleaning extended attrs"
-xattr -cr "./obj-aarch64-apple-darwin/dist/Glide.app"
+xattr -cr "./obj-$ARCH-apple-darwin/dist/Glide.app"
 
 step "Signing .app"
-./mach macos-sign -v -r -c "release" -e "production-without-restricted" -a "./obj-aarch64-apple-darwin/dist/Glide.app" --rcodesign-p12-file glideCert.p12 --rcodesign-p12-password-file glidePassword.passwd
+./mach macos-sign -v -r -c "release" -e "production-without-restricted" -a "./obj-$ARCH-apple-darwin/dist/Glide.app" --rcodesign-p12-file glideCert.p12 --rcodesign-p12-password-file glidePassword.passwd
 
 step "Creating DMG"
 ./mach python -m mozbuild.action.make_dmg \
@@ -76,18 +77,18 @@ step "Creating DMG"
   --background ./browser/branding/glide/background.png \
   --icon ./browser/branding/glide/firefox.icns \
   --dsstore ./browser/branding/glide/dsstore \
-  ./obj-aarch64-apple-darwin/dist/ ./glide-aarch64-temp.dmg
+  ./obj-$ARCH-apple-darwin/dist/ ./glide-$ARCH-temp.dmg
 
 step "Removing credentials"
 rm -f ./Glide_Browser.provisionprofile
 
 step "Signing DMG"
-hdiutil convert glide-aarch64-temp.dmg -format UDZO -imagekey zlib-level=9 -o glide.macos-aarch64.dmg
-codesign --deep -s "$APPLE_DEVELOPER_ID" glide.macos-aarch64.dmg
-xcrun notarytool submit "glide.macos-aarch64.dmg" \
+hdiutil convert glide-$ARCH-temp.dmg -format UDZO -imagekey zlib-level=9 -o glide.macos-$ARCH.dmg
+codesign --deep -s "$APPLE_DEVELOPER_ID" glide.macos-$ARCH.dmg
+xcrun notarytool submit "glide.macos-$ARCH.dmg" \
   --apple-id "$APPLE_ACCOUNT_ID" \
   --team-id "$APPLE_TEAM_ID" \
   --password "$APPLE_APP_ID_PASSWORD" \
   --no-s3-acceleration \
   --wait
-xcrun stapler staple "glide.macos-aarch64.dmg"
+xcrun stapler staple "glide.macos-$ARCH.dmg"
