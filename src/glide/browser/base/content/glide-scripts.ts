cd994c6c17f0cde0d4a5464042157b5abdec25fe
Robert Craigie <robert@craigie.dev>
2025-07-20 22:01:29 +0100

    fix(license): add license comment everywhere

--- src/glide/browser/base/content/glide-scripts.ts
+++ src/glide/browser/base/content/glide-scripts.ts
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 // On MacOS, Firefox loads *two* top-level windows, one with the standard `browser.xhtml` frame
 // that is loaded on every platform and a separate hidden window:
 //
--- src/glide/browser/base/content/utils/promises.mts
+++ src/glide/browser/base/content/utils/promises.mts
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 /**
  * A type representing the result of a Promise that has been settled (either fulfilled or rejected)
  * using the {@link all_settled} function.
