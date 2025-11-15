9b21283fd644f19fc05d3a7746deb739013152fa
Robert Craigie <robert@craigie.dev>
2025-11-15 15:06:04 +0000

    refactor(scroll/notification): use appmenu instead of drawer
    this avoids the page layout shifting due to the viewport getting smaller

--- src/glide/browser/base/content/test/scrolling/browser_scroll.ts
+++ src/glide/browser/base/content/test/scrolling/browser_scroll.ts
@@ -6,36 +6,41 @@
 
 const SCROLL_TEST_FILE = "http://mochi.test:8888/browser/glide/browser/base/content/test/excmds/scroll_test.html";
 
-add_task(async function test_scrolling_triggers_notification() {
-  Services.prefs.setBoolPref("glide.notifications.scroll_instant_to_smooth", false);
+const breaking_change_pref = "glide.notifications.scroll_instant_to_smooth";
+
+async function setup({ version }: { version: string } = { version: "0.1.52a" }) {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [breaking_change_pref, false],
+    ],
+  });
   await glide.fs.write(
     glide.path.join(glide.path.profile_dir, "glide__compatibility_oldest_version.txt"),
-    "0.1.52a_20251026025613/20251026025613",
+    `${version}_20251026025613/20251026025613`,
   );
   // call it manually here as testing on_startup things is tricky right now
   await GlideBrowser._setup_scroll_breaking_change_notification();
+  GlideBrowser.notify_scroll_breaking_change?.();
+}
+
+add_task(async function test_scrolling_triggers_notification() {
+  await setup();
 
   await BrowserTestUtils.withNewTab(SCROLL_TEST_FILE, async _ => {
     await keys("jjjjjj");
 
-    await new Promise((r) => setTimeout(r, 250));
+    const notification = await until(() =>
+      AppMenuNotifications.notifications.find((n) => n.id === "glide-smooth-scroll-default")
+    );
 
-    const notification = await until(() => gNotificationBox.getNotificationWithValue("glide-breaking-scroll"));
-
-    Assert.stringContains(notification.shadowRoot.textContent || "", "Smooth scrolling is now the default");
-
-    const link = notification.shadowRoot.querySelector(".message")!.children[0]! as HTMLAnchorElement;
     // rewrite the link to an allowed URL so mochitest doesn't complain about external requests.
-    link.href = "https://example.com/";
-    link.click();
+    notification.mainAction.docs_url = "https://example.com/";
+    notification.mainAction.callback();
 
-    await waiter(() => {
-      console.log(glide.ctx.url.toString());
-      return glide.ctx.url.toString() === "https://example.com/";
-    }).ok();
+    await waiter(() => glide.ctx.url.toString() === "https://example.com/").ok();
 
     notok(
-      gNotificationBox.getNotificationWithValue("glide-breaking-scroll"),
+      AppMenuNotifications.notifications.find((n) => n.id === "glide-smooth-scroll-default"),
       "notification should be removed after clicking it",
     );
 
@@ -45,8 +50,6 @@ add_task(async function test_scrolling_triggers_notification() {
   await BrowserTestUtils.withNewTab(SCROLL_TEST_FILE, async _ => {
     await keys("jjjjjj");
 
-    await new Promise((r) => setTimeout(r, 250));
-
     notok(
       gNotificationBox.getNotificationWithValue("glide-breaking-scroll"),
       "notification should not be triggered twice",
@@ -55,21 +58,13 @@ add_task(async function test_scrolling_triggers_notification() {
 });
 
 add_task(async function test_scrolling_does_NOT_trigger_notification_for_new_profile() {
-  Services.prefs.setBoolPref("glide.notifications.scroll_instant_to_smooth", false);
-  await glide.fs.write(
-    glide.path.join(glide.path.profile_dir, "glide__compatibility_oldest_version.txt"),
-    "0.1.54a_20251026025613/20251026025613",
-  );
-  // call it manually here as testing on_startup things is tricky right now
-  await GlideBrowser._setup_scroll_breaking_change_notification();
+  await setup({ version: "0.1.54a" });
 
   await BrowserTestUtils.withNewTab(SCROLL_TEST_FILE, async _ => {
     await keys("jjjjjj");
 
-    await new Promise((r) => setTimeout(r, 250));
-
     notok(
-      gNotificationBox.getNotificationWithValue("glide-breaking-scroll"),
+      AppMenuNotifications.notifications.find((n) => n.id === "glide-smooth-scroll-default"),
       "There should not be any notification if the oldest version a user used is greater than 0.1.53a",
     );
   });
