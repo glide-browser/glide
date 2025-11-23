41c7b0cf8435d80a03896fbb20b8b6ca1e90da1f
Robert Craigie <robert@craigie.dev>
2025-11-23 20:45:02 +0000

    refactor(tests): rename jumplist to navigation

---
+++ src/glide/browser/base/content/test/navigation/browser_urls.ts
@@ -0,0 +1,25 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+/* Any copyright is dedicated to the Public Domain.
+ * https://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+add_task(async function test_go_up() {
+  await GlideTestUtils.reload_config(function _() {});
+
+  await BrowserTestUtils.withNewTab(
+    "http://mochi.test:8888/browser/glide/browser/base/content/test/mode/input_test.html#foo?thing=true",
+    async _ => {
+      await keys("gu");
+      await waiter(() => glide.ctx.url.toString()).is(
+        "http://mochi.test:8888/browser/glide/browser/base/content/test/mode",
+      );
+
+      await keys("gu");
+      await waiter(() => glide.ctx.url.toString()).is("http://mochi.test:8888/browser/glide/browser/base/content/test");
+    },
+  );
+});
