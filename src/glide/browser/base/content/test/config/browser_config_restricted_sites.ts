f4a5974e06e7bcecdce8abff3fc7ef62cc4842ea
Robert Craigie <robert@craigie.dev>
2025-12-23 08:25:14 -0700

    fix(config): allow browser API on restricted pages (#125)

---
+++ src/glide/browser/base/content/test/config/browser_config_restricted_sites.ts
@@ -0,0 +1,86 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+/* Any copyright is dedicated to the Public Domain.
+ * https://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+declare var document: Document;
+declare var content: TestContent;
+
+add_task(async function test_executeScript__restricted_domain() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["extensions.webextensions.restrictedDomains", "example.com"]],
+  });
+
+  await GlideTestUtils.reload_config(function _() {
+    glide.keymaps.set("normal", "~", async ({ tab_id }) => {
+      const results = await browser.scripting.executeScript({
+        world: "MAIN",
+        func() {
+          document.body!.setAttribute("data-test-marker", "executed");
+          return document.body!.textContent!.trim();
+        },
+        target: { tabId: tab_id },
+      });
+      glide.g.value = results[0]?.result;
+      glide.g.test_checked = true;
+    });
+  });
+
+  await BrowserTestUtils.withNewTab(
+    "http://example.com/browser/docshell/test/browser/dummy_page.html",
+    async browser => {
+      await keys("~");
+      await waiter(() => glide.g.test_checked).ok();
+
+      const marker = await SpecialPowers.spawn(browser, [], () =>
+        content.document.body!.getAttribute("data-test-marker"));
+      is(marker, "executed", "Script should execute on webextensions restricted domain example.com");
+
+      is(glide.g.value, "just a dummy html file", "script return values should be propagated");
+    },
+  );
+});
+
+add_task(async function test_contentScript_uriFilters__restricted_domain() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["extensions.webextensions.restrictedDomains", "example.com"]],
+  });
+
+  await GlideTestUtils.reload_config(function _() {
+    glide.autocmds.create("ConfigLoaded", () => {
+      browser.contentScripts.register({
+        matches: ["*://example.com/*"],
+        runAt: "document_idle",
+        js: [{ code: `document.body.setAttribute("data-content-script", "injected");` }],
+      }).catch(() => {});
+      browser.contentScripts.register({
+        matches: ["*://should-not-match.com/*"],
+        runAt: "document_idle",
+        js: [{ code: `document.body.setAttribute("data-content-script2", "injected");` }],
+      }).catch(() => {});
+    });
+  });
+
+  await BrowserTestUtils.withNewTab(
+    "http://example.com/browser/docshell/test/browser/dummy_page.html",
+    async browser => {
+      await SpecialPowers.spawn(browser, [], async () => {
+        await ContentTaskUtils.waitForCondition(
+          () => content.document.body!.getAttribute("data-content-script") === "injected",
+          "Content script should inject on restricted domain via URI filter",
+        );
+      });
+
+      const second_marker = await SpecialPowers.spawn(browser, [], () =>
+        content.document.body!.getAttribute("data-content-script2"));
+      is(second_marker, null, "Second content script should not inject because the URI does not match");
+    },
+  );
+
+  // cleanup
+  await GlideTestUtils.reload_config(function _() {});
+});
